3879b0e29f0f4b7538cc6103fe568cf0bdd41d97
Dominic Ng <dominic.ng@coveinsurance.co.nz>
2019-04-22 17:00:41 +1200

    Add hooks and event handler

--- src/components/BusMovementInputSec.js
+++ src/components/BusMovementInputSec.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import TextInput from './formElements/TextInput';
 import Button from './formElements/Button';
 import FormContainer from './formElements/FormContainer';
 
 const BusMovementInputSec = () => {
+  const [movement, setMovement] = useState([]);
+  const [newOnChangeMovement, setNewOnChangeMovement] = useState('');
+  const [editMovementIndex, setEditMovementIndex] = useState();
+  const [editOnChangeMovement, setEditOnChangeMovement] = useState();
+
+  const newMoveRef = useRef();
+  const addBtnRef = useRef();
+
+  const addNewMovement = () => {
+    setMovement([...movement, newOnChangeMovement]);
+    setNewOnChangeMovement('');
+    newMoveRef.current.focus();
+  };
+
+  const deleteMovement = movementIndex => {
+    var filtered = movement.filter(function(value, index) {
+      return movementIndex !== index;
+    });
+
+    setMovement(filtered);
+  };
+
+  const loadEditMovement = index => {
+    setEditMovementIndex(index);
+    setEditOnChangeMovement(movement[index]);
+  };
+
+  const handleEnter = e => {
+    if (e.key === 'Enter') {
+      addBtnRef.current.click();
+    }
+  };
+
   return (
     <FormContainer
       onKeyPress={event => {
@@ -13,13 +46,39 @@ const BusMovementInputSec = () => {
       }}
     >
       <h3>Movements</h3>
-
+      {movement.map((move, index) => {
+        return (
+          <div className="form-group row" key={index}>
+            <>
+              <div className="col-md-3">{move}</div>
+              {typeof editMovementIndex === 'undefined' && (
+                <div className="col-md-3">
+                  <Button onClick={() => deleteMovement(index)} className="btn btn-danger">
+                    Delete
+                  </Button>
+                  <Button onClick={() => loadEditMovement(index)} className="btn btn-info">
+                    Edit
+                  </Button>
+                </div>
+              )}
+            </>
+          </div>
+        );
+      })}
       <div className="form-group row">
         <div className="col-md-3">
-          <TextInput placeholder="Add New Movement" />
+          <TextInput
+            refEle={newMoveRef}
+            placeholder="Add New Movement"
+            value={newOnChangeMovement}
+            onChange={e => setNewOnChangeMovement(e.target.value.toUpperCase())}
+            onKeyPress={e => handleEnter(e)}
+          />
         </div>
         <div className="col-md-3">
-          <Button className={'btn btn-primary'}>Add</Button>
+          <Button onClick={() => addNewMovement()} refEle={addBtnRef} className={'btn btn-primary'}>
+            Add
+          </Button>
         </div>
       </div>
     </FormContainer>
